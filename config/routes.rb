13898e9c33d1e7722d41e3edd9f7948665394808
ruas46 <guilhermeruas@hotmail.com>
2016-05-23 15:02:22 -0300

    Criando pagina de dicas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  
+  get '/dicas' => 'help#index'
+
+
+
+    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
