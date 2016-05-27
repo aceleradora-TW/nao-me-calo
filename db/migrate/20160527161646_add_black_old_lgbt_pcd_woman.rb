77f83d054587860d74d61ff5322c5d092fadb96b
ruas46 <guilhermeruas@hotmail.com>
2016-05-27 14:49:07 -0300

    #T2 <@williamcsa04,@mibaratto> - Deletado arquivo migrate 20160527161646_add_black_old_lgbt_pcd_woman.rb

--- db/migrate/20160527161646_add_black_old_lgbt_pcd_woman.rb
+++
@@ -1,9 +0,0 @@
-class AddBlackOldLgbtPcdWoman < ActiveRecord::Migration
-  def change
-    add_column :ratings, :black, :float
-    add_column :ratings, :old, :float
-    add_column :ratings, :woman, :float
-    add_column :ratings, :lgbt, :float
-    add_column :ratings, :pcd, :float
-  end
-end
