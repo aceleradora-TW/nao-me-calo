8c3be765223f2a9d66200cd6455ddf4cef2bf646
mathiasVoelcker <pedroschleder@outlook.com>
2016-05-30 11:13:52 -0300

    #5<@RenanAfrausino, @mathiasVoelcker> criando a listagem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,27 @@
 class WelcomeController < ApplicationController
   def index
+    @establishments = Establishment.create(
+    [{
+        name: "Beco",
+        address: "Exemplo"
+      },
+      {
+        name: "Beco",
+        address: "Exemplo"
+      },
+      {
+        name: "Beco",
+        address: "Exemplo"
+      },
+      {
+        name: "Beco",
+        address: "Exemplo"
+      },
+      {
+        name: "Beco",
+        address: "Exemplo"
+      }]
+    )
+
   end
 end
