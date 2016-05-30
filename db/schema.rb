6cfa2ba60655a16de25be1869655dcb175f08f41
ruas46 <guilhermeruas@hotmail.com>
2016-05-30 09:25:04 -0300

    Arrumando o CSS quebrado

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527174727) do
+ActiveRecord::Schema.define(version: 20160527162958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "establishments", force: :cascade do |t|
     t.string   "name"
@@ -36,53 +27,16 @@ ActiveRecord::Schema.define(version: 20160527174727) do
     t.datetime "updated_at",     null: false
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "overall_averages", force: :cascade do |t|
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "overall_avg",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
-  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.integer  "cacheable_id"
-    t.string   "cacheable_type"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
-
   create_table "ratings", force: :cascade do |t|
     t.float    "grade"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "lgbt"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+    t.integer  "establishment_id"
+    t.float    "black"
     t.float    "old"
     t.float    "woman"
-    t.integer  "black"
+    t.float    "lgbt"
     t.float    "pcd"
   end
 
