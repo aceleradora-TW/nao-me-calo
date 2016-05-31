963fb53e97e4e9d61c9218076e2d539dc262a807
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-05-31 11:05:17 -0300

    #0 <@ghig, @brandelli> - criado teste unitario para welcome

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+  describe "#index" do
+    before :each do
+      @est1 = Establishment.create!(name:"Teste", address:"Logo Ali", average_rating:5.0, lat:"0", lng:"0", id_places:"321")
+      @est2 = Establishment.create!(name:"Teste2", address:"Logo Ali2", average_rating:6.0, lat:"0", lng:"0", id_places:"121")
+    end
 
+    it 'expect to @establishments to be all establishments on DB' do
+      get :index
+      expect(assigns[:establishments]).to eq([@est1, @est2])
+    end
+  end
+
+  describe '#search' do
+    before :each do
+      @est1 = Establishment.create!(name:"Teste", address:"Logo Ali", average_rating:5.0, lat:"0", lng:"0", id_places:"321")
+    end
+
+    it 'expect to redirect to establishment when passing a existing ID' do
+      post :search, place_id_2: @est1.id_places
+      expect(response).to redirect_to(@est1)
+    end
+
+    it 'expect to flash error when establishment does not exist' do
+      post :search, place_id_2: nil
+      expect(flash[:error]).to eq('Lugar ainda não foi avaliado. Por favor, avalie abaixo.')
+    end
+  end
 end
