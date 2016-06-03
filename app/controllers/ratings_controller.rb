384815fcdf823649741ce8c6cc5bf36a007d9435
= <=>
2016-06-03 10:03:58 -0300

    #2 <@ghig, @silvia-marenco, @silvinho485> - Criando relação entre rating e establishment

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,6 @@
 class RatingsController < ApplicationController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
+  before_action :set_client, only: [:new, :create]
 
   # GET /ratings
   # GET /ratings.json
@@ -15,7 +16,7 @@ class RatingsController < ApplicationController
   # GET /ratings/new
   def new
     @rating = Rating.new
-    @client = GooglePlaces::Client.new("AIzaSyAJ6NOTnj_jq6jQ0vZPtosWhvoLnoLGlm8")
+    @place_id = params[:place_id]
 
     if params[:place_id] != nil
       @spot = @client.spot(params[:place_id])
@@ -32,6 +33,14 @@ class RatingsController < ApplicationController
   # POST /ratings.json
   def create
     @rating = Rating.new(rating_params)
+    @establishment = Establishment.search_by_id(params[:place_id]).first
+
+    if(@establishment.nil?)
+      @place = @client.spot(params[:place_id])
+      @establishment = Establishment.create!(name: @place.name, address: @place.formatted_address, lat: @place.lat, lng: @place.lng, id_places: @place.place_id)
+    end
+
+    @rating.establishment_id = @establishment.id
 
     respond_to do |format|
       if @rating.save
@@ -74,10 +83,14 @@ class RatingsController < ApplicationController
       @rating = Rating.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def rating_params
-      params.require(:rating).permit(:woman, :lgbtqia, :black, :disability, :elder, :obese, :name, :cpf, :email, :phone, :rating_date)
+    def set_client
+      @client = GooglePlaces::Client.new("AIzaSyAJ6NOTnj_jq6jQ0vZPtosWhvoLnoLGlm8")
     end
 
-    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def rating_params
+      params.require(:rating).permit(:woman, :lgbtqia, :black, :disability, :elder, :obese, :name, :cpf, :email, :phone, :rating_date, :establishment_id)
+    end
+
+
 end
