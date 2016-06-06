7e3ead765092c805cf84a5098c141ccaf1689974
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-06-06 14:18:18 -0300

    #0 <@ghig, @RenanAfrausino> - Consertando erro no JS

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 
   $('#dataAvaliacao').focusout(function(){
     validaData();
-
   });
 
   function showMessage(){
@@ -41,9 +40,10 @@ $(document).ready(function(){
       showMessage();
     }
   }
+
   function validaCPF(){
     var valor = 14;
-    if ($('#cpfAvaliacao').val() === "___.___.___-__" || $('#cpfAvaliacao').val() === "" || $('#cpfAvaliacao').val().length != valor){
+    if ($('#cpfAvaliacao').val() === "___.___.___-__" || $('#cpfAvaliacao').val() == "" || $('#cpfAvaliacao').val().length != valor || $('#cpfAvaliacao').val() == null){
       $('#cpfAvaliacao').addClass("error");
       errors["cpfAvaliacao"] = true;
       showMessage();
@@ -53,6 +53,7 @@ $(document).ready(function(){
       showMessage();
     }
   }
+
   function validaData(){
     var RegExPattern = /^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])      [\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$/;
     if ($('#dataAvaliacao').val() === "__/__/____" || $('#dataAvaliacao').val() == "" || !$('#dataAvaliacao').val().match(RegExPattern)){
@@ -65,12 +66,19 @@ $(document).ready(function(){
       showMessage();
     }
   }
-  $('#new_rating').submit(function(e){
-    validaNome();
-    validaCPF();
-    validaData();
-    if(!$('.errorDiv').hasClass("hidden")){
-      e.preventDefault();
+
+  function isNull(parameter){
+    if(parameter.val() == null || parameter.val() == ""){
+      return true;
+    } else {
+      return false;
     }
-  })
+  }
+
+  $('#new_rating').submit(function(e){
+    if(isNull($('#rating_name')) || isNull($('#cpfAvaliacao'))){
+      e.preventDefault();
+      validaCPF();
+    }
+  });
 });
