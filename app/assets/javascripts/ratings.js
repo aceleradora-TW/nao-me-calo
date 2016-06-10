f05e48bd3416967c53ec7c99677f3ac5c28d0bf2
Pedro <pedroschleder@outlook.com>
2016-06-10 14:52:57 -0300

    <@silvia-marenco, @pedrozan> - ajustando os campos da avaliação

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -149,4 +149,25 @@ $(document).ready(function(){
     enable = enableButtonTerms();
     show = showMessage();
   });
+
+  $.fn.setCursorPosition = function(pos) {
+    this.each(function(index, elem) {
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
+    });
+    return this;
+  };
+
+  $('.has-mask').click(function(e){
+    $('#cpfEvaluate').setCursorPosition(0);
+    $('#phoneEvaluate').setCursorPosition(0);
+    $('#dateEvaluate').setCursorPosition(0);
+  })
 });
