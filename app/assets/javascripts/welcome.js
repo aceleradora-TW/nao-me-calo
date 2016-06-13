99f4835429b3a61bbe1d036cfbc81348ff3d4f9f
william <williamcaetano14@gmail.com>
2016-06-13 09:45:02 -0300

    #10 <@brandelli, @pedrozan> - Fazendo o merge do MVP1 com o MVP2

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -64,3 +64,28 @@ function initAutocomplete () {
       }
     });
   });
+
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 12
+    });
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
