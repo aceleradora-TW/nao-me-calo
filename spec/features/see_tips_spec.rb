6ec01f1b97fc7c127343a1eadc1106208ec772db
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-06-13 15:21:29 -0300

    <#0 @mibaratto, @RenanAfrausino, @lucimarNeves> - Fazendo Capybara

--- spec/features/see_tips_spec.rb
+++ spec/features/see_tips_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
-feature "See tips" do
+feature "See tips", js: true do
   scenario "Click button of tips" do
     visit '/'
 
     click_link 'Dicas'
 
     expect(page).to have_content 'Sofri discriminação: e agora?'
+    sleep 5
+
   end
 end
