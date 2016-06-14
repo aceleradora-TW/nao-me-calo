0838e5567e92db0d711f5d4bec9e28615c4f385c
silvinho485 <silvinho485@gmail.com>
2016-06-14 13:29:04 -0300

    #02 <@silvinho485> - Limitando cpfs invalidos

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
     var Resto;
     Soma = 0;
 
-    if (strCPF == "00000000000") return true;
+    if ( strCPF == "00000000000" || strCPF == "11111111111" || strCPF == "22222222222" || strCPF == "33333333333" || strCPF == "44444444444" || strCPF == "55555555555" || strCPF == "66666666666" || strCPF == "77777777777" || strCPF == "88888888888" || strCPF == "99999999999" || strCPF == "01234567890") return true;
 
     for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
     Resto = (Soma * 10) % 11;
