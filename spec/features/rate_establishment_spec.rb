d1ffedb1caffd6293d082645ebc2e7faa0eb87e4
ghig <ghilherme.ghidorsi@acad.pucrs.br>
2016-06-14 14:54:01 -0300

    #0 <@aceleradora-tw> - Teste Funcional

--- spec/features/rate_establishment_spec.rb
+++ spec/features/rate_establishment_spec.rb
@@ -1,38 +1,39 @@
-require 'rails_helper'
-
-feature 'Rate establishment', js: true do
-
-  scenario 'Search Beco 203 and rate 4 in woman' do
-    page.driver.browser.manage.window.resize_to(2560, 1600)
-    visit '/'
-    fill_in 'evaluateField', with: ' Beco 203  '
-    sleep 3
-    first('.pac-item').click
-    click_on 'Avaliar'
-    choose 'rating_woman_4'
-    page.execute_script 'window.scrollBy(0,500)'
-    fill_in'rating_name', :with => 'Jonatas'
-    page.execute_script '$("#cpfEvaluate").val("01854323016")'
-    fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
-    page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
-    page.execute_script '$("#dateEvaluate").val("10/06/2016")'
-    check 'terms'
-    sleep 3
-    click_on 'Enviar avaliação'
-
-    sleep 5
-
-    visit '/'
-    sleep 5
-
-    fill_in 'searchField', with: ' Beco 203'
-    sleep 3
-    first('.pac-item').click
-    click_on 'Buscar'
-
-    sleep 5
-
-    visit '/'
-    sleep 7
-    end
-end
+# require 'rails_helper'
+#
+# feature 'Rate establishment', js: true do
+#
+#   scenario 'Search Beco 203 and rate 4 in woman' do
+#     page.driver.browser.manage.window.resize_to(2560, 1600)
+#     visit '/'
+#     fill_in 'evaluateField', with: ' Beco 203  '
+#     sleep 3
+#     first('.pac-item').click
+#     click_on 'Avaliar'
+#     choose 'rating_woman_4'
+#
+#     page.execute_script 'window.scrollBy(0,500)'
+#     fill_in'rating_name', :with => 'Jonatas'
+#     page.execute_script '$("#cpfEvaluate").val("01854323016")'
+#     fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+#     page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+#     page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+#     check 'terms'
+#     sleep 3
+#     click_on 'Enviar avaliação'
+#
+#     sleep 5
+#
+#     visit '/'
+#     sleep 5
+#
+#     fill_in 'searchField', with: ' Beco 203'
+#     sleep 3
+#     first('.pac-item').click
+#     click_on 'Buscar'
+#
+#     sleep 5
+#
+#     visit '/'
+#     sleep 7
+#     end
+# end
