b7a17f85187b2f46d955bd17f1f9d0ceda478533
cschallenberger <c.schallenberger@hotmail.com>
2016-06-21 15:04:41 -0300

    #13 <@brandelli,@cschallenberger,@pedrozan> - Feito envio do email para email da aceleradoratw9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            =>  "aceleradoratw9@gmail.com",
-    :password             =>  "naomecalo",
+    :user_name            =>  ENV['gmail_username'],
+    :password             =>  ENV['gmail_password'],
     :domain               => 'gmail.com',
     :authentication       => 'plain',
     :openssl_verify_mode  => 'none',
