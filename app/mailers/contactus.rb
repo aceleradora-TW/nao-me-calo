2493991fb1b339494e51ce4888963d2c03240e62
brandelli <bdbrandelli@hotmail.com>
2016-06-21 09:54:32 -0300

    #13 <@brandelli,@cschallenberger,@pedrozan> - Teste de email do fale conosco

--- app/mailers/contactus.rb
+++ app/mailers/contactus.rb
@@ -2,6 +2,6 @@ class ContactUs < ApplicationMailer
   def welcome_email()
     # @user = user
     # @url  = 'http://example.com/login'
-    mail(to: 'pedrofilosofo@gmail.com', subject: 'Welcome to My Awesome Site')
+    mail(to: 'aceleradoratw9@gmail.com', subject: 'Welcome to My Awesome Site')
   end
 end
