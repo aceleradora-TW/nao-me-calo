dda0ec37a53b641a1b17475fd8b705178293107f
brandelli <bdbrandelli@hotmail.com>
2016-06-21 10:40:35 -0300

    #13 <@brandelli,@cschallenberger,@pedrozan> - Teste de email do fale conosco 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
+    :user_name            => "aceleradoratw9@gmail.com", # ENV['gmail_username'],
+    :password             => "naomecalo", # ENV['gmail_password'],
+    :domain               => 'gmail.com',
+    :authentication       => :login,
     :enable_starttls_auto => true
   }
 
