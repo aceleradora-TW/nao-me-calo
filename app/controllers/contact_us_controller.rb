36045b62c967d7b945dc2219d5c4925d10a5d429
Pedro Henrique <pedroschleder@outlook.com>
2016-06-21 15:52:12 -0300

    #13 <@brandelli, @pedrozan> - Fazendo Capybara para contact_us

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -1,14 +1,16 @@
 class ContactUsController < ApplicationController
   def index
-    end
+  end
 
-    def send_mail
-      @message = params[:message]
-      @email = params[:email]
-      @subject = params[:subject]
-      @name = params[:name]
-      ContactUs.welcome_email(@name, @email, @subject, @message).deliver_now
-      redirect_to "/faleconosco"
+  def send_mail
+    @message = params[:message]
+    @email = params[:email]
+    @subject = params[:subject]
+    @name = params[:name]
+    ContactUs.welcome_email(@name, @email, @subject, @message).deliver_now
+    respond_to do |format|
+      format.html { redirect_to "/faleconosco", notice: 'Mensagem enviada com sucesso!' }
     end
+  end
 
 end
