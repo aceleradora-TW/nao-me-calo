e1c3b1593eb5f074d8e085cce20edfe069ef44d7
RenanAfrausino <renan.afrausino@gmail.com>
2016-06-24 10:24:25 -0300

    #0 <@RenanAfrausino, @mathiasVoelcker> - teste o bug fale consco

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,29 +1,57 @@
+$(document).ready(function(){
+  $('#button_disable').prop("disabled", true);
 
-function checkMail(){
-  var emailExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var emailInput = $('#emailEvaluate').val();
+  function disabled_button(){
+    for(var l in errors){
+      if(errors[l] === true){
+        //  $('.errorDiv').removeClass("hidden");
+        $('#button_disable').prop("disabled", true);
+        return;
+      }
+    }
+    if(enableCheckBox()){
+      $('#checkBox').prop("disabled", false);
+    }
 
-  if (!emailInput.match(emailExp) || emailInput === ""){
-    $('#emailEvaluate').addClass("error");
-    errors["emailEvaluate"] = true;
-    showMessage();
-  }else{
-    $('#emailEvaluate').removeClass("error");
-    errors["emailEvaluate"] = false;
-    showMessage();
   }
 
-}
 
-function checkMessage(){
-  var message = $('#messageEvaluate').val();
-  if (message === ""){
-    $('#messageEvaluate').addClass("error");
-    errors["messageEvaluate"] = true;
-    showMessage();
-  }else{
-    $('#messageEvaluate').removeClass("error");
-    errors["messageEvaluate"] = false;
-    showMessage();
+  function enableCheckBox(){
+    if(!(isNull($('#emailEvaluate')) || isNull($('#messageEvaluate')))){
+      if($('#checkBox:checked').length === 1){
+        $('#button_disable').prop("disabled", false);
+        return true;
+      } else {
+        $('#button_disable').prop("disabled", true);
+        return false;
+      }
+    }
+  }
+  function checkMail(){
+    var emailExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailInput = $('#emailEvaluate').val();
+
+    if (!emailInput.match(emailExp) || emailInput === ""){
+      $('#emailEvaluate').addClass("error");
+      errors["emailEvaluate"] = true;
+      showMessage();
+    }else{
+      $('#emailEvaluate').removeClass("error");
+      errors["emailEvaluate"] = false;
+      showMessage();
+    }
+  }
+
+  function checkMessage(){
+    var message = $('#messageEvaluate').val();
+    if (message === ""){
+      $('#messageEvaluate').addClass("error");
+      errors["messageEvaluate"] = true;
+      showMessage();
+    }else{
+      $('#messageEvaluate').removeClass("error");
+      errors["messageEvaluate"] = false;
+      showMessage();
+    }
   }
 }
