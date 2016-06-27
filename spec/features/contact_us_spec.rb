42d5bcdf2a85231ec8c2779dc6560cc90ee8dcec
cschallenberger <c.schallenberger@hotmail.com>
2016-06-27 10:44:41 -0300

    #0 <@cschallenberger, @pedrozan> - Capybara do contact_us arrumado

--- spec/features/contact_us_spec.rb
+++ spec/features/contact_us_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
-feature "Send email", js: true do
+feature "Send email" do # , :js => true do
   scenario "Fill all fields and send" do
-    # visit '/faleconosco'
-    # fill_in 'name', :with => 'John'
-    # fill_in 'email', :with => 'john@gmail.com'
-    # fill_in 'subject', :with => 'Some subject'
-    # fill_in 'message', :with => 'Some mesage'
-    # click_button 'Enviar!'
-    # expect(page).to have_content 'Mensagem enviada com sucesso!'
+    visit '/faleconosco'
+    fill_in 'name', :with => 'John'
+    fill_in 'email', :with => 'john@gmail.com'
+    fill_in 'subject', :with => 'Some subject'
+    fill_in 'message', :with => 'Some mesage'
+    click_on 'Enviar!'
+    expect(page).to have_content 'Mensagem enviada com sucesso!'
   end
 end
