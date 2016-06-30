37092408ad1f984960d9abcc8d7b0c151f4a4f47
silvinho485 <silvinho485@gmail.com>
2016-06-30 14:17:22 -0300

    #13 <@brandelli, @silvinho485> - consertado bugs do 'fale conosco'

--- app/assets/javascripts/contact_us.js
+++ app/assets/javascripts/contact_us.js
@@ -4,62 +4,68 @@ $(document).ready(function(){
   initialize();
 
 
-$('#emailEvaluate').focusout(function(){
-  checkMail();
-});
+  $('#emailEvaluateContact').focusout(function(){
+    checkMailContact();
+  });
 
-$('#messageEvaluate').focusout(function(){
-  checkMessage();
-});
+  $('#messageEvaluate').focusout(function(){
+    checkMessage();
+  });
 
-function disableButton(){
-  $('#button_disable').attr('disabled', true);
-}
+  function disableButton(){
+    $('#button_disable').attr('disabled', true);
+  }
 
-function enableButton(){
-  $('#button_disable').attr('disabled', false);
-}
+  function enableButton(){
+    $('#button_disable').attr('disabled', false);
+  }
 
-function initialize(){
-  for(var l in errorsContact){
-    if(errorsContact[l] === true){
-      return disableButton();
-    } else {
-      enableButton();
+  function initialize(){
+    for(var l in errorsContact){
+      if(errorsContact[l] === true){
+        return disableButton();
+      } else {
+        enableButton();
+      }
     }
   }
-}
-function checkMail(){
-  var emailExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var emailInput = $('#emailEvaluate').val();
 
-  if (!emailInput.match(emailExp) || emailInput === "" ){
-    $('#emailEvaluate').addClass("error");
-    $('#emailAlert').removeClass("hidden");
-    $('#emailAlert').addClass("textFieldError");
-    $('#emailEvaluate').removeClass("hidden");
-    errorsContact["emailEvaluate"] = true;
-    initialize();
-  }else{
-    $('#emailEvaluate').removeClass("error");
-    errorsContact["emailEvaluate"] = false;
-    initialize();
-  }
-}
+  function checkMailContact(){
+    var emailExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailInput = $('#emailEvaluateContact').val();
 
-function checkMessage(){
-  var message = $('#messageEvaluate').val();
-  if (message === ""){
-    $('#messageEvaluate').addClass("error");
-    $('#messageAlertText').removeClass("hidden");
-    $('#messageAlert').addClass("textFieldError");
-    $('#messageEvaluate').removeClass("hidden");
-    errorsContact["messageEvaluate"] = true;
-    initialize();
-  }else{
-    $('#messageEvaluate').removeClass("error");
-    errorsContact["messageEvaluate"] = false;
-    initialize();
+    if (!emailInput.match(emailExp) || emailInput === "" ){
+      $('#emailEvaluateContact').addClass("error");
+      $('#emailAlertContact').removeClass("hidden");
+      $('#emailAlertTextContact').removeClass("hidden");
+      $('#emailAlertContact').addClass("textFieldError");
+      $('#emailEvaluateContact').removeClass("hidden");
+      errorsContact["emailEvaluateContact"] = true;
+      initialize();
+    }else{
+      $('#emailEvaluateContact').removeClass("error");
+      $('#emailAlertContact').removeClass("textFieldError");
+      $('#emailAlertTextContact').addClass("hidden");
+      errorsContact["emailEvaluateContact"] = false;
+      initialize();
+    }
   }
-}
+
+  function checkMessage(){
+    var message = $('#messageEvaluate').val();
+    if (message === ""){
+      $('#messageEvaluate').addClass("error");
+      $('#messageAlertText').removeClass("hidden");
+      $('#messageAlert').addClass("textFieldError");
+      $('#messageEvaluate').removeClass("hidden");
+      errorsContact["messageEvaluate"] = true;
+      initialize();
+    }else{
+      $('#messageEvaluate').removeClass("error");
+      $('#messageAlert').removeClass("textFieldError");
+      $('#messageAlertText').addClass("hidden");
+      errorsContact["messageEvaluate"] = false;
+      initialize();
+    }
+  }
 });
