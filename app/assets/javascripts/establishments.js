eca0564982c7a52666b4f43f9ded09f47aa8c730
WilliamCSA04 <williamcaetano14@gmail.com>
2016-07-01 16:00:17 -0300

    #6 <@mathiasVoelcker, @WIlliamCSA04> - arrumada lista do perfil

--- app/assets/javascripts/establishments.js
+++ app/assets/javascripts/establishments.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
     var x = 5;
-    var size_li = $("#list-last-5 li").size();
-    $('#list-last-5 li:lt('+size_li+')').hide();
-    $('#list-last-5 li:lt('+x+')').show();
-    $('#show_five_button').click(function(){
+    var size_li = $("#listLast5 li").size();
+    $('#listLast5 li:lt('+size_li+')').hide();
+    $('#listLast5 li:lt('+x+')').show();
+
+    $('#showFiveButton').click(function(){
       x = (x+5 <= size_li) ? x+5 : size_li;
-      $('#list-last-5 li:lt('+x+')').show();
+      $('#listLast5 li:lt('+x+')').show();
 
     });
 });
