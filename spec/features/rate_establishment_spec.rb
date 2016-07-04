2031fff5fb6391e5233a2f87526cbefd566085c5
silvinho485 <silvinho485@gmail.com>
2016-07-04 11:12:34 -0300

    #6 <@silvinho485 @pedrozan> - teste bem sucedido do avaliar local

--- spec/features/rate_establishment_spec.rb
+++ spec/features/rate_establishment_spec.rb
@@ -1,39 +1,21 @@
-# require 'rails_helper'
-#
-# feature 'Rate establishment', js: true do
-#
-#   scenario 'Search Beco 203 and rate 4 in woman' do
-#     page.driver.browser.manage.window.resize_to(2560, 1600)
-#     visit '/'
-#     fill_in 'evaluateField', with: ' Beco 203  '
-#     sleep 3
-#     first('.pac-item').click
-#     click_on 'Avaliar'
-#     choose 'rating_woman_4'
-#
-#     page.execute_script 'window.scrollBy(0,500)'
-#     fill_in'rating_name', :with => 'Jonatas'
-#     page.execute_script '$("#cpfEvaluate").val("01854323016")'
-#     fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
-#     page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
-#     page.execute_script '$("#dateEvaluate").val("10/06/2016")'
-#     check 'terms'
-#     sleep 3
-#     click_on 'Enviar avaliação'
-#
-#     sleep 5
-#
-#     visit '/'
-#     sleep 5
-#
-#     fill_in 'searchField', with: ' Beco 203'
-#     sleep 3
-#     first('.pac-item').click
-#     click_on 'Buscar'
-#
-#     sleep 5
-#
-#     visit '/'
-#     sleep 7
-#     end
-# end
+require 'rails_helper'
+
+feature 'Rate establishment', js: true do
+
+  scenario 'rate 4 in woman' do
+    visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+    find_by_id('rate_test').click
+    page.execute_script 'window.scrollBy(0,500)'
+    fill_in'nameEvaluate', :with => 'Jonatas'
+    page.execute_script '$("#cpfEvaluate").val("01854323016")'
+    fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+    page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+    page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+    fill_in 'rating_description', :with => 'Este é um texto de teste!'
+    check 'terms'
+    click_on 'Enviar avaliação'
+
+    expect(page).to have_content 'Avaliação feita com sucesso'
+
+  end
+end
