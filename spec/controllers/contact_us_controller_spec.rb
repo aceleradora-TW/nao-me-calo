0739498a5a2030f67f6c71bf27b59987c10b8ac3
WilliamCSA04 <williamcaetano14@gmail.com>
2016-07-04 11:45:02 -0300

    #13 <@williamcsa04, @RenanAfausino> - Criado primeiro teste da pagina Fale Conosco

--- spec/controllers/contact_us_controller_spec.rb
+++ spec/controllers/contact_us_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ContactUsController, type: :controller do
 
+
+  describe "#send_mail" do
+
+
+    it "expect to flash notice when the rating is create " do
+
+      post :send_mail
+      {
+        message: "teste",
+        email: "teste@teste.com",
+        subject: "teste",
+        name: "teste",
+      }
+
+      expect(flash[:notice]).to eq('Mensagem enviada com sucesso!')
+    end
+
+  
+
+  end
+
 end
