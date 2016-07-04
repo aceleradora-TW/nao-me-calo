40dfeb29a2caaf94d3512a53d829ad60e1bd8c2a
silvinho485 <silvinho485@gmail.com>
2016-07-04 15:01:43 -0300

    #6 <@silvinho485, @RenanAfrausino> - Inserindo teste de cpf e data invalidos

--- spec/features/rate_establishment_spec.rb
+++ spec/features/rate_establishment_spec.rb
@@ -3,49 +3,80 @@ require 'rails_helper'
 feature 'Rate establishment', js: true do
 
   scenario 'rate 4 in woman' do
-    visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
-    find_by_id('rate_test').click
-    page.execute_script 'window.scrollBy(0,500)'
-    fill_in'nameEvaluate', :with => 'Jonatas'
-    page.execute_script '$("#cpfEvaluate").val("01854323016")'
-    fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
-    page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
-    page.execute_script '$("#dateEvaluate").val("10/06/2016")'
-    fill_in 'ratingDescription', :with => 'Este é um texto de teste!'
-    check 'terms'
-    click_on 'Enviar avaliação'
+   visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+   find_by_id('rate_test').click
+   page.execute_script 'window.scrollBy(0,500)'
+   fill_in'nameEvaluate', :with => 'Jonatas'
+   page.execute_script '$("#cpfEvaluate").val("01854323016")'
+   fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+   page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+   page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+   fill_in 'ratingDescription', :with => 'Este é um texto de teste!'
+   check 'terms'
+   click_on 'Enviar avaliação'
 
-    expect(page).to have_content 'Avaliação feita com sucesso'
+   expect(page).to have_content 'Avaliação feita com sucesso'
 
-  end
+ end
 
-  scenario 'failed rating' do
-    visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
-    # find_by_id('rate_test').click
-    page.execute_script 'window.scrollBy(0,500)'
-    fill_in'nameEvaluate', :with => 'Jonatas'
-    page.execute_script '$("#cpfEvaluate").val("01854323016")'
-    fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
-    page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
-    page.execute_script '$("#dateEvaluate").val("10/06/2016")'
-    fill_in 'ratingDescription', :with => 'Este é um texto de teste!'
-    check 'terms'
-    expect(page).to have_button('submitButton', disabled: true)
-  end
+ scenario 'failed rating' do
+   visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+   # find_by_id('rate_test').click
+   page.execute_script 'window.scrollBy(0,500)'
+   fill_in'nameEvaluate', :with => 'Jonatas'
+   page.execute_script '$("#cpfEvaluate").val("01854323016")'
+   fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+   page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+   page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+   fill_in 'ratingDescription', :with => 'Este é um texto de teste!'
+   check 'terms'
+   expect(page).to have_button('submitButton', disabled: true)
+ end
 
-  scenario 'rating with ofensive words' do
-    visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
-    find_by_id('rate_test').click
-    page.execute_script 'window.scrollBy(0,500)'
-    fill_in'nameEvaluate', :with => 'Jonatas'
-    page.execute_script '$("#cpfEvaluate").val("01854323016")'
-    fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
-    page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
-    page.execute_script '$("#dateEvaluate").val("10/06/2016")'
-    fill_in 'ratingDescription', :with => 'merda'
-    check 'terms'
-    expect(page).to have_button('submitButton', disabled: true)
-    expect(page).to have_content 'Seu texto contém palavras ofensivas, por favor corrija'
-  end
+ scenario 'rating with ofensive words' do
+   visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+   find_by_id('rate_test').click
+   page.execute_script 'window.scrollBy(0,500)'
+   fill_in'nameEvaluate', :with => 'Jonatas'
+   page.execute_script '$("#cpfEvaluate").val("01854323016")'
+   fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+   page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+   page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+   fill_in 'ratingDescription', :with => 'merda'
+   check 'terms'
+   expect(page).to have_button('submitButton', disabled: true)
+   expect(page).to have_content 'Seu texto contém palavras ofensivas, por favor corrija'
+ end
+
+ scenario 'filling cpf field with invalid' do
+   visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+   find_by_id('rate_test').click
+   page.execute_script 'window.scrollBy(0,500)'
+   fill_in'nameEvaluate', :with => 'Jonatas'
+   fill_in 'cpfEvaluate', :with => '11111111111'
+   fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+   page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+   page.execute_script '$("#dateEvaluate").val("10/06/2016")'
+   fill_in 'ratingDescription', :with => 'merda'
+   check 'terms'
+   expect(page).to have_button('submitButton', disabled: true)
+   expect(page).to have_content 'Por favor, digite um CPF válido'
+ end
+
+ scenario 'rating wrongDate' do
+   visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
+   find_by_id('rate_test').click
+   page.execute_script 'window.scrollBy(0,500)'
+   fill_in'nameEvaluate', :with => 'Jonatas'
+   page.execute_script '$("#cpfEvaluate").val("01854323016")'
+   fill_in 'emailEvaluate', :with => 'jonatas@terra.com.br'
+   page.execute_script '$("#phoneEvaluate").val("(51)33306088")'
+   page.execute_script '$("#dateEvaluate").val("10/06/2017")'
+   fill_in 'ratingDescription', :with => 'merda'
+   check 'terms'
+   expect(page).to have_button('submitButton', disabled: true)
+   expect(page).to have_content 'Seu texto contém palavras ofensivas, por favor corrija'
+ end
+
 
 end
