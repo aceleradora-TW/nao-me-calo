d8586e7981f1cc384b283f33ec553bc4b3c29878
Pedro <pedroschleder@outlook.com>
2016-07-05 13:01:41 -0300

    #20 <@brandelli, @pedrozan> - Criada pagina para modelar relato

---
+++ app/admin/ratings.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Rating do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :woman, :description
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+index do
+  selectable_column
+  id_column
+  column :description
+  column :race
+  actions
+end
+
+end
