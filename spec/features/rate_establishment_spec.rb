42a250d2d4b56b52a34d59cc982dec551923ef6b
William <c.schallenberger@hotmail.com>
2016-07-06 15:48:59 -0300

    #T4 <@cschallenberger, @lucimarNeves> - Arrumado layout

--- spec/features/rate_establishment_spec.rb
+++ spec/features/rate_establishment_spec.rb
@@ -16,7 +16,7 @@ feature 'Rate establishment', js: true do
    click_on 'Enviar avaliação'
 
    expect(page).to have_content 'Avaliação feita com sucesso'
-
+   sleep 5
  end
 
  scenario 'failed rating' do
@@ -31,6 +31,7 @@ feature 'Rate establishment', js: true do
    fill_in 'ratingDescription', :with => 'Este é um texto de teste!'
    check 'terms'
    expect(page).to have_button('submitButton', disabled: true)
+   sleep 5
  end
 
  scenario 'rating with ofensive words' do
@@ -46,11 +47,13 @@ feature 'Rate establishment', js: true do
    check 'terms'
    expect(page).to have_button('submitButton', disabled: true)
    expect(page).to have_content 'Seu texto contém palavras ofensivas, por favor corrija'
+   sleep 5
  end
 
  scenario 'filling cpf field with invalid' do
    visit '/avaliar?utf8=%E2%9C%93&commit=Avaliar&placeId=ChIJj2J3iGOCGZUR5XT38N2pANQ'
    find_by_id('rate_test').click
+   sleep 3
    page.execute_script 'window.scrollBy(0,500)'
    fill_in'nameEvaluate', :with => 'Jonatas'
    fill_in 'cpfEvaluate', :with => '11111111111'
@@ -61,6 +64,7 @@ feature 'Rate establishment', js: true do
    check 'terms'
    expect(page).to have_button('submitButton', disabled: true)
    expect(page).to have_content 'Por favor, digite um CPF válido'
+   sleep 5
  end
 
  scenario 'rating wrongDate' do
@@ -76,6 +80,7 @@ feature 'Rate establishment', js: true do
    check 'terms'
    expect(page).to have_button('submitButton', disabled: true)
    expect(page).to have_content 'Seu texto contém palavras ofensivas, por favor corrija'
+   sleep 5
  end
 
 
