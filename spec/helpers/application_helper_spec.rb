9a225bd610bb66116b0d7666e440db9b14511d02
williamcsa04 <mathias_V94@hotmail.com>
2016-07-19 13:19:08 -0300

    <@mathiasVoelcker> - clean code

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,15 +11,4 @@ RSpec.describe ApplicationHelper, type: :helper do
     @ratings5 = [@r5, @r5]
   end
 
-  it 'expect to populate rate result with ratings and pessimo' do
-    expect(populate_rate_array(@ratings1)).to eq([[@r1, "Péssimo"], [@r1, "Péssimo"]])
-  end
-
-  it 'expect to populate rate result with rating and regular' do
-    expect(populate_rate_array(@ratings3)).to eq([[@r3, "Regular"], [@r3, "Regular"]])
-  end
-
-  it 'expect to populate with rating and otimo' do
-    expect(populate_rate_array(@ratings5)).to eq([[@r5, "Ótimo"], [@r5, "Ótimo"]])
-  end
 end
--- spec/models/establishment_spec.rb
+++ spec/models/establishment_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Establishment, type: :model do
+  it 'expect to populate rate result with ratings and pessimo' do
+    expect(populate_rate_array(@ratings1)).to eq([[@r1, "Péssimo"], [@r1, "Péssimo"]])
+  end
+
+  it 'expect to populate rate result with rating and regular' do
+    expect(populate_rate_array(@ratings3)).to eq([[@r3, "Regular"], [@r3, "Regular"]])
+  end
+
+  it 'expect to populate with rating and otimo' do
+    expect(populate_rate_array(@ratings5)).to eq([[@r5, "Ótimo"], [@r5, "Ótimo"]])
+  end
 end
