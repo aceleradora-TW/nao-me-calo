4e3d6fc2fe4bb75a5869c2898bdbc857d25bfa94
ruas46 <williamcaetano14@gmail.com>
2016-07-22 14:19:12 -0300

    #0 <@williamcsa04> - Posicionando botões de compartilhar na pagina de ranking para baixo da lista

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,11 +9,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            =>  ENV['aceleradoratw9@gmail.com'],
-    :password             =>  ENV['GMAIL_PASSWORD'],
+    :user_name            =>  "aceleradoratw9@gmail.com",
+    :password             =>  "naomecalo",
     :domain               => 'gmail.com',
     :authentication       => 'plain',
-    :openssl_verify_mode  => 'none',
     :enable_starttls_auto => true
   }
 
