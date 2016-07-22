094e97e3896314bd3a413e1e7d51938e304184b8
williamcsa04 <mathias_V94@hotmail.com>
2016-07-22 10:11:02 -0300

    <@mathiasVoelcker, @brandelli> - arrumado bug no ranking welcome

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,15 +1,5 @@
 module WelcomeHelper
 
-  def set_concept_for_welcome
-    array = Ranking.generate_array_ranking(@establishments)
-    places = []
-    array.take(5).each do |establishment|
-      places << [establishment[0], establishment[1]]
-    end
-    places.each do |place|
-      place[1] = Concept.determine_concept(place[1])
-    end
-  end
 
   def generate_ranking
     array = Ranking.generate_array_ranking(@establishments)
--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -11,16 +11,14 @@ class Establishment < ActiveRecord::Base
   def has_more_than_2_ratings?
     total_ratings_moderated = 0
     self.ratings.each do |rating|
-      if rating.moderated == true
         total_ratings_moderated += 1
-      end
     end
 
     return total_ratings_moderated > 2
   end
 
   def calculate_average
-    all_ratings = self.ratings.to_a.delete_if {|rating| rating.moderated == false}
+    all_ratings = self.ratings.to_a
     all_ratings = all_ratings.each.map do |rating|
       rating.average_rating
     end
