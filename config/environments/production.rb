eb3bb6018b1ecabf3be097e83118109747489b66
ruas46 <williamcaetano14@gmail.com>
2016-07-22 14:40:15 -0300

    #0 <@williamcsa04> - voltando configurações de produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,11 +9,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            =>  "aceleradoratw9@gmail.com",
-    :password             =>  "naomecalo",
+    :user_name            =>  ENV['GMAIL_EMAIL'],
+    :password             =>  ENV['GMAIL_PASSWORD'],
     :domain               => 'gmail.com',
     :authentication       => 'plain',
-    :enable_starttls_auto => true
   }
 
   # Eager load code on boot. This eager loads most of Rails and
