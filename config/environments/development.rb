323611d6305a2e9107e550014ef1cda6a8d3bb0c
ruas46 <guilhermeruas@hotmail.com>
2016-08-09 15:16:52 -0300

    #0 <@williamcsa04, @jonatassalgado> - Trocando API google

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  G_PLACE_KEY = "AIzaSyAJ6NOTnj_jq6jQ0vZPtosWhvoLnoLGlm8"
+  G_PLACE_KEY = "AIzaSyAr1tsRiNhjTSSTamqCFMTpIctBJsMbsVA"
 
   # Do not eager load code on boot.
   config.eager_load = false
